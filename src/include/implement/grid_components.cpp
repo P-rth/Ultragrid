f14283932b7ea77be321fe8bc0a6094dcd7c8fa6
p-rth <parthsahni09@gmail.com>
2025-03-13 00:45:17 +0530

    bigicon first implementation, gridcomponents refactor

--- src/include/implement/grid_components.cpp
+++ src/include/implement/grid_components.cpp
@@ -5,11 +5,13 @@
 #include "ftxui/screen/color.hpp"  // for Color, Color::Blue, Color::Red
 #include "ftxui/dom/node.hpp"      // for Render
 #include <cmath>
+#include <ftxui/component/component_base.hpp>
 #include <iostream>
 #include <ostream>
 #include <vector>
 
 #include "./headers/helpers.hpp"
+#include "./headers/art.hpp"
 
 
 using namespace ftxui;
@@ -20,149 +22,156 @@ using namespace std::chrono_literals;
 using TicTacToeButton_Options_Grid = std::vector<std::vector<std::vector<int>>>;  //a vector for all x*y buttons to store options format :: [int disabled, int blink]  --> 0/1 (int used so that can add more features in future)
 
 class TicTacToeButton {
-private:
-    int& gridValue;
-    int row;
-    int col;
-    bool isHovered = false;
-    bool isPressed = false;
-    bool click_release_edge = false;
-    std::vector<int>& options;
-    ftxui::Box box_;
-    ftxui::Component button;
+    private:
+        int& gridValue;
+        int row;
+        int col;
+        bool isHovered = false;
+        bool isPressed = false;
+        bool click_release_edge = false;
+        std::vector<int>& options;
+        ftxui::Box box_;
+        ftxui::Component button;
 
-    std::string getSymbolString(int value) const {
-        switch(value) {
-            case 1: return "X";
-            case 2: return "O";
-            default: return " ";
+        std::string getSymbolString(int value) const {
+            switch(value) {
+                case 1: return "X";
+                case 2: return "O";
+                default: return " ";
+            }
         }
-    }
 
-    ftxui::Component makeButton() {
-        bool isdisabled = options[0] == 1;  // 0 = not disabled
-        std::cout << isdisabled << " OPTION" << std::endl;
+        ftxui::Component makeButton() {
+            bool isdisabled = options[0] == 1;  // 0 = not disabled
+            std::cout << isdisabled << " OPTION" << std::endl;
 
-        using namespace ftxui;
+            using namespace ftxui;
 
-        auto buttonComponent = Button(" ", [this, isdisabled] {
-            if (!isdisabled && gridValue == 0) {
-                gridValue = 1;
-            }
-        });
+            auto buttonComponent = Button(" ", [this, isdisabled] {
+                if (!isdisabled && gridValue == 0) {
+                    gridValue = 1;
+                }
+            });
 
-        buttonComponent |= CatchEvent([this, buttonComponent, isdisabled](Event event) {
-            const bool mouse_in_button = box_.Contain(event.mouse().x, event.mouse().y);
+            buttonComponent |= CatchEvent([this, buttonComponent, isdisabled](Event event) {
+                const bool mouse_in_button = box_.Contain(event.mouse().x, event.mouse().y);
 
-            if (isdisabled && buttonComponent->Focused()) {
-                return false;
-            }
+                if (isdisabled && mouse_in_button) {
+                    return false;
+                }
 
-            if (mouse_in_button != isHovered) {
-                buttonComponent->TakeFocus();
-                isHovered = mouse_in_button;
-                return true;
-            }
+                if (mouse_in_button != isHovered) {
+                    buttonComponent->TakeFocus();
+                    isHovered = mouse_in_button;
+                // return true;
+                }
 
-            if (event.mouse().button == Mouse::Left) {
-                if (event.mouse().motion == Mouse::Pressed) {
-                    if (mouse_in_button) {
-                        isPressed = true;
-                        return true;
+                if (event.mouse().button == Mouse::Left) {
+                    if (event.mouse().motion == Mouse::Pressed) {
+                        if (mouse_in_button) {
+                            isPressed = true;
+                            return true;
+                        }
+                        else {
+                            isPressed = false;
+                        }
                     }
-                    else {
+                    else if (event.mouse().motion == Mouse::Released && mouse_in_button) {
+                        if (isPressed) {
+                            click_release_edge = true;
+                            return true;
+                        }
+                    }
+                    else if (event.mouse().motion == Mouse::Released) {
                         isPressed = false;
                     }
                 }
-                else if (event.mouse().motion == Mouse::Released && mouse_in_button) {
-                    if (isPressed) {
-                        click_release_edge = true;
-                        return true;
+
+                return false;
+            });
+
+            return Renderer(buttonComponent, [this, buttonComponent, isdisabled] {
+                auto content = text(getSymbolString(gridValue)) |
+                            center |
+                            size(WIDTH, EQUAL, 3);
+
+                if (isdisabled) {
+                    if (buttonComponent->Focused()) {
+                        content = content | dim | bgcolor(ftxui::Color::Grey50);
+                    } else {
+                        content = content | dim | bgcolor(ftxui::Color::Grey30);
+
+                    }
+
+                }
+
+                if (buttonComponent->Focused() && !isdisabled) {
+                    if (click_release_edge && gridValue == 0) {
+                        gridValue = 1;
+                        content = content | bgcolor(ftxui::Color::Red);
+                    }
+                    else {
+                        if (isPressed && gridValue == 0) {
+                            content = content | bgcolor(ftxui::Color::OrangeRed1);
+                        }
+                        else if (gridValue == 0) {
+                            content = content | bgcolor(ftxui::Color::GrayDark);
+                        }
+                        else if (gridValue == 1) {
+                            content = content | color(ftxui::Color::Red);
+                        }
+                        else if (gridValue == 2) {
+                            content = content | color(ftxui::Color::Blue);
+                        }
                     }
                 }
-                else if (event.mouse().motion == Mouse::Released) {
-                    isPressed = false;
-                }
-            }
 
-            return false;
-        });
-
-        return Renderer(buttonComponent, [this, buttonComponent, isdisabled] {
-            auto content = text(getSymbolString(gridValue)) |
-                          center |
-                          size(WIDTH, EQUAL, 3);
-
-            if (isdisabled) {
-                content = content | dim | bgcolor(ftxui::Color::Grey35);
-            }
-
-            if (buttonComponent->Focused() && !isdisabled) {
-                if (click_release_edge && gridValue == 0) {
-                    gridValue = 1;
-                    content = content | bgcolor(ftxui::Color::Red);
-                }
-                else {
-                    if (isPressed && gridValue == 0) {
-                        content = content | bgcolor(ftxui::Color::OrangeRed1);
-                    }
-                    else if (gridValue == 0) {
-                        content = content | bgcolor(ftxui::Color::GrayDark);
-                    }
-                    else if (gridValue == 1) {
-                        content = content | color(ftxui::Color::Red);
-                    }
-                    else if (gridValue == 2) {
-                        content = content | color(ftxui::Color::Blue);
-                    }
-                }
-            }
-
-            click_release_edge = false;
-            return content | reflect(box_);
-        });
-    }
-
-public:
-    TicTacToeButton(int& gridRef, int r, int c, std::vector<int>& opts)
-        : gridValue(gridRef), row(r), col(c), options(opts) {
-        button = makeButton();
-    }
-
-    void TakeFocus_btn() {
-        //std::cout << "TicTacToeButton: Attempting to take focus" << std::endl;
-        if (!button) {
-            std::cout << "TicTacToeButton: Button component is null!" << std::endl;
-            return;
+                click_release_edge = false;
+                return content | reflect(box_);
+            });
         }
-        button->TakeFocus();
-    }
 
-    ftxui::Component GetButton() const {
-        return button;
-    }
+    public:
+        TicTacToeButton(int& gridRef, int r, int c, std::vector<int>& opts)
+            : gridValue(gridRef), row(r), col(c), options(opts) {
+            button = makeButton();
+        }
 
-    int GetRow() const { return row; }
-    int GetCol() const { return col; }
+        void TakeFocus_btn() {
+            //std::cout << "TicTacToeButton: Attempting to take focus" << std::endl;
+            if (!button) {
+                std::cout << "TicTacToeButton: Button component is null!" << std::endl;
+                return;
+            }
+            button->TakeFocus();
+        }
+
+        ftxui::Component GetButton() const {
+            return button;
+        }
+
+        int GetRow() const { return row; }
+        int GetCol() const { return col; }
 };
 
 
 class SmallGrid {
     private:
         std::vector<std::vector<std::unique_ptr<TicTacToeButton>>> buttons;
-        int selected_x = 0;  // Shared x coordinate for horizontal navigation
-        int selected_y = 0;  // y coordinate for vertical navigation
+        int selected_x = 0;
+        int selected_y = 0;
         int& disabled;
+        int bigicon = 0;
         std::vector<int> buttonOptions;
+        ftxui::Component gridComponent; // Store the grid component
 
     public:
-        std::vector<std::vector<int>> grid;    // 0 = empty, 1 = X, 2 = O
+        std::vector<std::vector<int>> grid;
 
-        SmallGrid(int& d) : disabled(d) , buttonOptions{d} {
-
+        SmallGrid(int& d) : disabled(d), buttonOptions{d} {
             grid = {{0, 0, 0},
-                    {0, 0, 0},
-                    {0, 0, 0}};
+                   {0, 0, 0},
+                   {0, 0, 0}};
 
             // Initialize buttons
             buttons.resize(3);
@@ -172,50 +181,69 @@ class SmallGrid {
                     buttons[i][j] = std::make_unique<TicTacToeButton>(grid[i][j], i, j, buttonOptions);
                 }
             }
+
+            // Initialize grid component
+            gridComponent = makeGridComponent();
         }
 
         std::vector<std::vector<int>>* getGridRef() {
-                return &grid;
-            }
+            return &grid;
+        }
 
         void takefocus_sm_grid(int x = 1, int y = 1) {
-
-        //  std::cout << "Taking focus at (" << x << ", " << y << ")  smallgrid" << std::endl;
             buttons[x][y]->TakeFocus_btn();
         }
 
+        // Getter for the grid component
+        ftxui::Component getGridComponent() {
+            return gridComponent;
+        }
+
+        void setbigicon(int bigicon_set) {
+            this->bigicon = bigicon_set;
+            // Recreate grid component when bigicon changes
+            gridComponent = makeGridComponent();
+        }
+
+    private:
         ftxui::Component makeGridComponent() {
             using namespace ftxui;
-            auto container = Container::Vertical({},&selected_y);
+            Component container;
 
-            for (int i = 0; i < 3; i++) {
-                auto row = Container::Horizontal({},&selected_x);
-                for (int j = 0; j < 3; j++) {
-                    // Use the TicTacToeButton class
-                    row->Add(buttons[i][j]->GetButton());
+            if (bigicon == 0) {
+                container = Container::Vertical({}, &selected_y);
 
-                    // Add vertical separator except after last column
-                    if (j < 2) {
-                        row->Add(Renderer([]{
+                for (int i = 0; i < 3; i++) {
+                    auto row = Container::Horizontal({}, &selected_x);
+                    for (int j = 0; j < 3; j++) {
+                        row->Add(buttons[i][j]->GetButton());
+
+                        if (j < 2) {
+                            row->Add(Renderer([]{
+                                return separatorCharacter("·");
+                            }));
+                        }
+                    }
+
+                    container->Add(row);
+
+                    if (i < 2) {
+                        container->Add(Renderer([]{
                             return separatorCharacter("·");
                         }));
                     }
                 }
-
-                container->Add(row);
-
-                // Add horizontal separator except after last row
-                if (i < 2) {
-                    container->Add(Renderer([]{
-                        return separatorCharacter("·");
-                    }));
-                }
+            }
+            else if (bigicon == 1) {
+                container = Renderer([&]{ return nonWrappingParagraph(bigx); });
+            }
+            else if (bigicon == 2) {
+                container = Renderer([&]{ return nonWrappingParagraph(bigx); });
             }
 
             return container;
         }
 
-        // Keep getSymbolString if needed elsewhere in SmallGrid
         std::string getSymbolString(int value) const {
             switch(value) {
                 case 1: return "X";
@@ -227,11 +255,13 @@ class SmallGrid {
 
 
 
+
 class LargeGrid {
     private:
         std::vector<std::vector<std::unique_ptr<SmallGrid>>> grids;
         int selected_x = 0;
         int selected_y = 0;
+        Component mainComponent;
 
 
 
@@ -261,6 +291,8 @@ class LargeGrid {
                 }
             }
 
+            mainComponent = makeGridComponent();
+
         }
 
         int getValue(int bigRow, int bigCol, int smallRow, int smallCol) const {
@@ -287,35 +319,44 @@ class LargeGrid {
             return values;
         }
 
-        ftxui::Component makeGridComponent() {
-            using namespace ftxui;
-            auto container = Container::Vertical({}, &selected_y);
+        Component makeGridComponent() {
+                    using namespace ftxui;
+                    auto container = Container::Vertical({}, &selected_y);
 
-            for(int i = 0; i < 3; i++) {
-                auto row = Container::Horizontal({}, &selected_x);
+                    for(int i = 0; i < 3; i++) {
+                        auto row = Container::Horizontal({}, &selected_x);
 
-                for(int j = 0; j < 3; j++) {
-                    row->Add(grids[i][j]->makeGridComponent());
+                        for(int j = 0; j < 3; j++) {
+                            row->Add(grids[i][j]->getGridComponent());
 
-                    // Add vertical separator between SmallGrids
-                    if (j < 2) {
-                        row->Add(Renderer([]{
-                            return separatorDouble();
-                        }));
+                            if (j < 2) {
+                                row->Add(Renderer([]{
+                                    return separatorDouble();
+                                }));
+                            }
+                        }
+
+                        container->Add(row);
+
+                        if (i < 2 ) {
+                            container->Add(Renderer([]{
+                                return separatorDouble();
+                            }));
+                        }
                     }
-                }
 
-                container->Add(row);
+                    mainComponent = container; // Store the component
+                    return mainComponent;
+        }
 
-                // Add horizontal separator between rows
-                if (i < 2 ) {
-                    container->Add(Renderer([]{
-                        return separatorDouble();
-                    }));
-                }
-            }
 
-            return container;
+        ftxui::Component getComponent() {
+            return mainComponent;
+        }
+
+        void setbigicon_big(int x = 0, int y = 0, int icon = 0) {
+            grids[x][y]->setbigicon(icon);
+            makeGridComponent(); // Rebuild the component
         }
 
         void takefocus_big(int x = 1, int y = 1,int z = 1,int w = 1) {
--- src/include/implement/ultragrid.cpp
+++ src/include/implement/ultragrid.cpp
@@ -1,9 +1,11 @@
 #include <cstdlib>
 #include <ftxui/component/component.hpp>
+#include <ftxui/component/component_base.hpp>
 #include <ftxui/component/component_options.hpp>
 #include <ftxui/component/screen_interactive.hpp>
 #include <ftxui/dom/elements.hpp>
 #include <ftxui/screen/string.hpp>
+#include <iostream>
 #include <string>
 #include "./headers/helpers.hpp"
 
@@ -19,35 +21,44 @@ void ultragrid_start_singleplayer() {
     using namespace ftxui;
     auto screen = ScreenInteractive::Fullscreen();
 
-    LargeGrid grid;
+    LargeGrid gridcmp;
+    Component grid = gridcmp.getComponent();
+    Component gridholder = Container::Vertical({grid});
 
-    auto container = grid.makeGridComponent();
 
-    container -> Add(Button("EXIT",screen.ExitLoopClosure()));
+    // Create a vertical container for all components
+    auto main_container = Container::Vertical({});
 
-    container -> Add(Button("focus",[&grid]() {
+    // Add the grid component
+    main_container->Add(gridholder);
+
+    // Add buttons
+    main_container->Add(Button("EXIT", screen.ExitLoopClosure()));
+
+    main_container->Add(Button("focus", [&gridcmp]() {
         std::cout << "focus" << std::endl;
-        grid.takefocus_big(0,0,0,0);
+        gridcmp.takefocus_big(0,0,0,0);
     }));
 
+    main_container->Add(Button("bigicon", [&gridcmp,&grid, &gridholder]() {
+        std::cout << "bigicon" << std::endl;
+        gridcmp.setbigicon_big(2,2,2);
+        // Update the container
+        grid->Detach();
+        grid = gridcmp.getComponent();              //update the main big grid
+        gridholder->Add(grid);
+    }));
 
-
-    auto renderer = Renderer(container, [&] {
+    auto renderer = Renderer(main_container, [&] {
         return vbox({
             text("Tic-Tac-Toe") | bold | center,
             separator(),
-            container->Render() | border|hcenter|vcenter,
-
+            main_container->Render() | border | hcenter | vcenter,
         });
     });
 
     screen.Loop(renderer);
 
-    largegrid_val grid4d = grid.get4DArray();
+    largegrid_val grid4d = gridcmp.get4DArray();
     largegrid_to_cout(grid4d);
-
-
-
-    std::cout << "hello "<< std::endl;
-
 }
